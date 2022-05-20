247ffd9e90de44ffd7062705c716804f5abeea8b
comanchechao <61477751+comanchechao@users.noreply.github.com>
2022-05-20 17:47:08 +0330

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
       },
     ],
   },
+  target: 'static',
   loading: '~/components/loading.vue',
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
